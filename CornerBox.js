0de6360a30f4e9c79f725d902bcb16254acd2275
Jero Soler <jero.soler@gmail.com>
2021-02-18 20:40:51 +0100

    Fix lenght border && version 0.0.3

--- CornerBox.js
+++ CornerBox.js
@@ -13,37 +13,37 @@ class cornerBox {
       const lineBox = parseInt(props.get(`--cornerbox-length`)); 
       const lineBoxwidth = parseInt(props.get(`--cornerbox-width`));
       const colorBox = props.get(`--cornerbox-color`).toString().trim(); 
-      
-      ctx.lineWidth = lineBox;
-      ctx.beginPath();
+      if(lineBox != 0) {
+        ctx.lineWidth = lineBox;
+        ctx.beginPath();
 
-      /* UP Left */ 
-      ctx.moveTo(0, 0);
-      ctx.lineTo(0, lineBoxwidth);
-      ctx.moveTo(0, 0);
-      ctx.lineTo(lineBoxwidth, 0);
+        /* UP Left */ 
+        ctx.moveTo(0, 0);
+        ctx.lineTo(0, lineBoxwidth);
+        ctx.moveTo(0, 0);
+        ctx.lineTo(lineBoxwidth, 0);
 
-      /* Up Right */
-      ctx.moveTo(size.width-lineBoxwidth, 0);
-      ctx.lineTo(size.width, 0);
-      ctx.moveTo(size.width, 0);
-      ctx.lineTo(size.width, lineBoxwidth);
+        /* Up Right */
+        ctx.moveTo(size.width-lineBoxwidth, 0);
+        ctx.lineTo(size.width, 0);
+        ctx.moveTo(size.width, 0);
+        ctx.lineTo(size.width, lineBoxwidth);
 
-      /* Down Left */
-      ctx.moveTo(0, size.height-lineBoxwidth);
-      ctx.lineTo(0, size.height);
-      ctx.moveTo(0, size.height);
-      ctx.lineTo(lineBoxwidth, size.height); 
+        /* Down Left */
+        ctx.moveTo(0, size.height-lineBoxwidth);
+        ctx.lineTo(0, size.height);
+        ctx.moveTo(0, size.height);
+        ctx.lineTo(lineBoxwidth, size.height); 
 
-      /* Down Right */
-      ctx.moveTo(size.width, size.height-lineBoxwidth);
-      ctx.lineTo(size.width, size.height);
-      ctx.moveTo(size.width, size.height);
-      ctx.lineTo(size.width-lineBoxwidth, size.height);
+        /* Down Right */
+        ctx.moveTo(size.width, size.height-lineBoxwidth);
+        ctx.lineTo(size.width, size.height);
+        ctx.moveTo(size.width, size.height);
+        ctx.lineTo(size.width-lineBoxwidth, size.height);
 
-      ctx.strokeStyle = colorBox;
-      ctx.stroke();
-      
+        ctx.strokeStyle = colorBox;
+        ctx.stroke();
+      }      
     }
   }
   
